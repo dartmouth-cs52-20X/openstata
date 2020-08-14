ed0f5aa740195469193d98bc8b45547dccf2c2d7
Jack Keane <john.f.keane.22@dartmouth.edu>
2020-08-14 17:53:20 -0600

    connect to backend

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import AppBar from '@material-ui/core/Appbar';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Person from '@material-ui/icons/Person';
+import Computer from '@material-ui/icons/Computer';
+import axios from 'axios';
 
 const MUIAppBar = () => {
   return (
@@ -20,6 +22,20 @@ const MUIAppBar = () => {
 };
 
 const Welcome = (props) => {
+  const [vars, setVars] = useState(0);
+  const [title, setTitle] = useState('none');
+
+  const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+  const ROOT_URL = 'http://openstata-api.herokuapp.com';
+
+  const callAPI = () => {
+    axios.get(`${PROXY_URL}${ROOT_URL}/data`).then((result) => {
+      console.log('API:', result.data.data_result);
+      setVars(result.data.data_result[0].num_vars);
+      setTitle(result.data.data_result[0].title);
+    });
+  };
+
   return (
     <div>
       <MUIAppBar />
@@ -30,6 +46,13 @@ const Welcome = (props) => {
           <Typography variant="body1">Sign Up</Typography>
           <Person />
         </IconButton>
+        <IconButton onClick={callAPI}>
+          <Typography variant="body1">Call API</Typography>
+          <Computer />
+        </IconButton>
+        <p>
+          Title: {title}. Vars: {vars}
+        </p>
       </div>
     </div>
   );
