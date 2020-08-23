f8aa8c05e896bf5b4413574cf12b045f07dee45d
Jack Keane <john.f.keane.22@dartmouth.edu>
2020-08-22 22:16:19 -0600

    build basic editor layout

--- src/pages/codeEditor.js
+++ src/pages/codeEditor.js
@@ -1,13 +1,60 @@
 import React, { useState } from 'react';
-import Editor from 'react-simple-code-editor';
 import axios from 'axios';
-import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-jsx';
+import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/theme-github';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: 'grey',
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: 'grey',
+    display: 'flex',
+    flexDirection: 'row',
+    height: '100vh',
+  },
+}));
 
 function CodeEditor() {
+  const classes = useStyles();
   const [code, setCode] = useState('');
   const [compilation, setCompilation] = useState('');
+
+  const doFiles = ['Inbox', 'Starred', 'Send email', 'Drafts'];
+  const otherData = ['All mail', 'Trash', 'Spam'];
 
   const runCode = () => {
     axios
@@ -22,24 +69,69 @@ function CodeEditor() {
 
   return (
     <div className="editorView">
-      <div className="comp">
-        <p>{compilation}</p>
-      </div>
-      <div>
-        <Editor
-          value={code}
-          onValueChange={(newCode) => setCode(newCode)}
-          padding={20}
-          highlight={(newCode) => highlight(newCode, languages.js)}
-          style={{
-            fontFamily: '"Fira code", "Fira Mono", monospace',
-            fontSize: 12,
-          }}
-          id="editor"
-        />
-        <button type="button" onClick={() => runCode()}>
-          Run code
-        </button>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {doFiles.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {otherData.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <div className={classes.content}>
+        <div className="comp">
+          <p>{compilation}</p>
+        </div>
+        <div className="divider" />
+        <div className="editorContainer">
+          <AppBar position="static">
+            <Grid container direction="row" justify="flex-end">
+              <IconButton onClick={() => runCode()}>
+                <Typography variant="body1">Run Code</Typography>
+                <PlayArrow />
+              </IconButton>
+            </Grid>
+          </AppBar>
+          <AceEditor
+            placeholder="Enter code here"
+            mode="xml"
+            theme="github"
+            onChange={(newCode) => setCode(newCode)}
+            fontSize={14}
+            value={code}
+            setOptions={{
+              enableBasicAutocompletion: false,
+              enableLiveAutocompletion: false,
+              enableSnippets: false,
+              showLineNumbers: true,
+              tabSize: 2,
+            }}
+            height="100%"
+            width="100%"
+          />
+        </div>
       </div>
     </div>
   );
