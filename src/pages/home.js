08e026c5ddb266663bc41eab02eecad390772dd4
Jack Keane <john.f.keane.22@dartmouth.edu>
2020-09-01 13:40:02 -0600

    change home buttons from tabs to list

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,21 +3,19 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-/* import List from '@material-ui/core/List';
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import FolderIcon from '@material-ui/icons/Folder'; */
+import DescriptionIcon from '@material-ui/icons/Description';
+import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
+import BookIcon from '@material-ui/icons/Book';
+import BookOutlinedIcon from '@material-ui/icons/BookOutlined';
 import Fab from '@material-ui/core/Fab';
 import { NavLink } from 'react-router-dom';
 import Drawer from '@material-ui/core/Drawer';
 
 import NavBar from '../components/navbar';
-// import TutorialOptions from '../components/tutorialOptions';
 
 import { getDoFiles, createDoFile, getTutorialFiles } from '../actions';
 
@@ -27,112 +25,6 @@ const mapStateToProps = (reduxState) => ({
 });
 
 const drawerWidth = 248;
-
-// temporary until we set up the database...
-/* const data = {
-  content: {
-    tutorials: [
-      {
-        mod: {
-          name: 'Introduction',
-          options: [
-            {
-              tutorialName: 'Lesson 1.1: Getting Started',
-              content: 'tutorial goes here',
-            },
-            {
-              tutorialName: 'Lesson 1.2: Basic Commands',
-              content: 'tutorial goes here',
-            },
-            {
-              tutorialName: 'Lesson 1.3: Data Import/Export',
-              content: 'tutorial goes here',
-            },
-          ],
-        },
-      },
-      {
-        mod: {
-          name: 'Intermediate',
-          options: [
-            {
-              tutorialName: 'Lesson 2.1: Data Transformation Commands',
-              content: 'tutorial goes here',
-            },
-            {
-              tutorialName: 'Lesson 2.1: Data Analysis Commands',
-              content: 'tutorial goes here',
-            },
-            {
-              tutorialName: 'Lesson 2.1: Graphing Commands',
-              content: 'tutorial goes here',
-            },
-          ],
-        },
-      },
-      {
-        mod: {
-          name: 'Bonus',
-          options: [
-            {
-              tutorialName: 'Lesson 3.1: Bonus 1',
-              content: 'tutorial goes here',
-            },
-            {
-              tutorialName: 'Lesson 3.2: Bonus 2',
-              content: 'tutorial goes here',
-            },
-            {
-              tutorialName: 'Lesson 3.3: Bonus 3',
-              content: 'tutorial goes here',
-            },
-          ],
-        },
-      },
-    ],
-
-    projects: [
-      {
-        mod: {
-          name: 'My Do Files',
-          options: [
-            {
-              projectName: 'Hello world',
-              content: 'My first file',
-            },
-            {
-              projectName: 'Test file',
-              content: 'Some other file',
-            },
-            {
-              projectName: 'Example file',
-              content: 'A file',
-            },
-          ],
-        },
-      },
-      {
-        mod: {
-          name: 'Other Do Files',
-          options: [
-            {
-              projectName: 'Project Mod 2 Project 1',
-              content: 'Project goes here',
-            },
-            {
-              projectName: 'Project Mod 2 Project 2',
-              content: 'Project goes here',
-            },
-            {
-              projectName: 'Project Mod 2 Project 3',
-              content: 'Project goes here',
-            },
-          ],
-        },
-      },
-    ],
-  },
-}; */
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -147,54 +39,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function TabPanel(props) {
-  // eslint-disable-next-line object-curly-newline
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={0}>
-          <Typography component="span">{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-/* function populateProjectModules() {
-  return (
-    <List className="listItem" component="nav" aria-label="projects">
-      {data.content.projects.map((key) => (
-        <ListItem button onClick={() => console.log('hello')}>
-          <ListItemIcon>
-            <FolderIcon />
-          </ListItemIcon>
-          <ListItemText primary={key.mod.name} />
-        </ListItem>
-      ))}
-    </List>
-  );
-} */
-
 function HomePage(props) {
   const classes = useStyles();
-  const [value, setValue] = useState(0);
   const [isTutorial, setIsTutorial] = useState(null);
-  // const [displayModule, setDisplayModule] = useState('');
   const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
@@ -263,23 +110,6 @@ function HomePage(props) {
       });
   }
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-
-    if (newValue === 1) {
-      setIsTutorial(true);
-    } else {
-      setIsTutorial(false);
-    }
-  };
-
-  const tabStyle = {
-    minWidth: 124,
-    paddingLeft: 10,
-    paddingRight: 10,
-    backgroundColor: 'white',
-  };
-
   return (
     <div>
       <NavBar className={classes.appBar} page="home" />
@@ -292,19 +122,24 @@ function HomePage(props) {
               paper: classes.drawerPaper,
             }}
           >
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="simple tabs example"
-            >
-              <Tab style={tabStyle} label="Tutorials" {...a11yProps(0)} />
-              <Tab style={tabStyle} label="Do Files" {...a11yProps(1)} />
-            </Tabs>
-            {!isTutorial ? (
-              <TabPanel value={value} index={0} />
-            ) : (
-              <TabPanel value={value} index={1} />
-            )}
+            <List>
+              <ListItem button key={1} onClick={() => setIsTutorial(false)}>
+                <ListItemIcon>
+                  {!isTutorial ? <BookIcon /> : <BookOutlinedIcon />}
+                </ListItemIcon>
+                <ListItemText primary="Tutorials" />
+              </ListItem>
+              <ListItem button key={2} onClick={() => setIsTutorial(true)}>
+                <ListItemIcon>
+                  {isTutorial ? (
+                    <DescriptionIcon />
+                  ) : (
+                    <DescriptionOutlinedIcon />
+                  )}
+                </ListItemIcon>
+                <ListItemText primary="Do Files" />
+              </ListItem>
+            </List>
           </Drawer>
         </div>
         {!isTutorial ? (
